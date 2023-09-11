b9a611be26012091ddcd1d18d9b57f6d1e890a2f
NavyyBlue <jean.cardenas2@unmsm.edu.pe>
2023-09-10 22:24:25 -0500

    Applying MVVM

--- app/src/main/java/com/navyblue/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/navyblue/rickandmortyapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.lifecycle.ViewModelProvider
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.picasso.Picasso
@@ -18,6 +19,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: SharedViewModel by lazy {
+        ViewModelProvider(this).get(SharedViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,33 +35,23 @@ class MainActivity : AppCompatActivity() {
         val originText = findViewById<AppCompatTextView>(R.id.originLabelTextView)
         val speciesText = findViewById<AppCompatTextView>(R.id.speciesLabelTextView)
 
-        rickAndMortyService.getCharacterById(54)
-            .enqueue(object : Callback<GetCharacterByIdResponse> {
-                override fun onResponse(
-                    call: Call<GetCharacterByIdResponse>,
-                    response: Response<GetCharacterByIdResponse>
-                ) {
-                    Log.i("MainActivity", response.toString())
-                    if (!response.isSuccessful) {
-                        Toast.makeText(this@MainActivity, "Error network call", Toast.LENGTH_SHORT)
-                            .show()
-                        return
-                    }
-                    val body = response.body()!!
-                    nameText.text = body.name
-                    statusText.text = body.status
-                    originText.text = body.origin.name
-                    speciesText.text = body.species
-                    if (body.gender.equals("male", true))
-                        genderImage.setImageResource(R.drawable.ic_male)
-                    else genderImage.setImageResource(R.drawable.ic_female)
-                    Picasso.get().load(body.image).into(headerImage)
 
-                }
+        viewModel.refreshCharacter(54)
+        viewModel.characterByIdLiveData.observe(this) { response ->
+            if (response == null) {
+                Toast.makeText(this@MainActivity, "Error network call", Toast.LENGTH_SHORT).show()
+                return@observe
+            }
 
-                override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
-                    Log.i("MainActivity02", t.message ?: "Null message")
-                }
-            })
+            nameText.text = response.name
+            statusText.text = response.status
+            originText.text = response.origin.name
+            speciesText.text = response.species
+            if (response.gender.equals("male", true))
+                genderImage.setImageResource(R.drawable.ic_male)
+            else genderImage.setImageResource(R.drawable.ic_female)
+            Picasso.get().load(response.image).into(headerImage)
+
+        }
     }
 }
