30d6cad8a1874b19739528837e46da34cc115f5a
NavyyBlue <jean.cardenas2@unmsm.edu.pe>
2023-09-11 11:52:42 -0500

    Dynamic background color

--- app/src/main/java/com/navyblue/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/navyblue/rickandmortyapp/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.navyblue.rickandmortyapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.picasso.Picasso
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import androidx.palette.graphics.Palette
+import com.squareup.picasso.Callback
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,9 +29,10 @@ class MainActivity : AppCompatActivity() {
         val genderImage = findViewById<AppCompatImageView>(R.id.genderImageView)
         val originText = findViewById<AppCompatTextView>(R.id.originLabelTextView)
         val speciesText = findViewById<AppCompatTextView>(R.id.speciesLabelTextView)
+        val mainView = findViewById<ConstraintLayout>(R.id.MainView)
 
 
-        viewModel.refreshCharacter(54)
+        viewModel.refreshCharacter(6)
         viewModel.characterByIdLiveData.observe(this) { response ->
             if (response == null) {
                 Toast.makeText(this@MainActivity, "Error network call", Toast.LENGTH_SHORT).show()
@@ -50,8 +46,26 @@ class MainActivity : AppCompatActivity() {
             if (response.gender.equals("male", true))
                 genderImage.setImageResource(R.drawable.ic_male)
             else genderImage.setImageResource(R.drawable.ic_female)
-            Picasso.get().load(response.image).into(headerImage)
+
+            Picasso.get().load(response.image)
+                .into(headerImage, object : Callback {
+                    override fun onSuccess() {
+                        val bitmap = (headerImage.drawable as BitmapDrawable).bitmap
+                        val palette = Palette.from(bitmap).generate()
+                        val dominantColor = palette.getDominantColor(0x00FFFFFF)
+
+                        mainView.setBackgroundColor(dominantColor)
+                        window.statusBarColor = dominantColor
+                    }
+
+                    override fun onError(e: Exception?) {
+                        Toast.makeText(this@MainActivity, "Error network call", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
 
         }
     }
+
+
 }
